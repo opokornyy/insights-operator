1559fc49b3aba39d06531ec157a1e032de5fde77
Ondrej Pokorny <opokorny@redhat.com>
2026-02-10 15:14:18 +0100

    feat: add tests for install-config gatherer
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- pkg/gatherers/clusterconfig/cluster_config_v1_config_map_test.go
+++ pkg/gatherers/clusterconfig/cluster_config_v1_config_map_test.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"testing"
 
+	installertypes "github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/baremetal"
+	"github.com/openshift/installer/pkg/types/gcp"
+	"github.com/openshift/installer/pkg/types/vsphere"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,11 +40,102 @@ func Test_gatherClusterConfigV1(t *testing.T) {
 	assert.NoError(t, err)
 
 	installConfig := `baseDomain: ""
-metadata:
-  creationTimestamp: null
+metadata: {}
 platform: {}
 pullSecret: ""
 `
 
 	assert.Equal(t, installConfig, string(data))
+}
+
+func Test_anonymizeVSphere(t *testing.T) {
+	platform := &vsphere.Platform{
+		FailureDomains: []vsphere.FailureDomain{
+			{
+				Topology: vsphere.Topology{
+					Datacenter: "datacenter-1",
+				},
+			},
+		},
+		VCenters: []vsphere.VCenter{
+			{
+				Username: "admin@vsphere.local",
+				Password: "SuperSecretPassword123",
+			},
+		},
+	}
+
+	anonymizeVSphere(platform)
+
+	// Verify failure domains are anonymized
+	assert.Len(t, platform.FailureDomains, 1)
+	assert.NotEqual(t, "datacenter-1", platform.FailureDomains[0].Topology.Datacenter)
+	assert.NotEmpty(t, platform.FailureDomains[0].Topology.Datacenter)
+
+	// Verify vCenter credentials are anonymized
+	assert.Len(t, platform.VCenters, 1)
+	assert.NotEqual(t, "admin@vsphere.local", platform.VCenters[0].Username)
+	assert.NotEqual(t, "SuperSecretPassword123", platform.VCenters[0].Password)
+	assert.NotEmpty(t, platform.VCenters[0].Username)
+	assert.NotEmpty(t, platform.VCenters[0].Password)
+}
+
+func Test_anonymizeGCPConfig(t *testing.T) {
+	platform := &gcp.Platform{
+		Region:    "us-central1",
+		ProjectID: "my-project-12345",
+		DNS: &gcp.DNS{
+			PrivateZone: &gcp.DNSZone{
+				ProjectID: "dns-project-xyz",
+			},
+		},
+	}
+
+	anonymizeGCPConfig(platform)
+
+	// Verify main fields are anonymized
+	assert.NotEqual(t, "us-central1", platform.Region)
+	assert.NotEqual(t, "my-project-12345", platform.ProjectID)
+	assert.NotEmpty(t, platform.Region)
+	assert.NotEmpty(t, platform.ProjectID)
+
+	// Verify DNS private zone is anonymized
+	assert.NotNil(t, platform.DNS)
+	assert.NotNil(t, platform.DNS.PrivateZone)
+	assert.NotEqual(t, "dns-project-xyz", platform.DNS.PrivateZone.ProjectID)
+	assert.NotEmpty(t, platform.DNS.PrivateZone.ProjectID)
+}
+
+func Test_anonymizeInstallConfig_BareMetal(t *testing.T) {
+	installConfig := &installertypes.InstallConfig{
+		Platform: installertypes.Platform{
+			BareMetal: &baremetal.Platform{
+				Hosts: []*baremetal.Host{
+					{
+						BMC: baremetal.BMC{
+							Username: "admin",
+							Password: "password123",
+						},
+					},
+					{
+						BMC: baremetal.BMC{
+							Username: "root",
+							Password: "secret456",
+						},
+					},
+				},
+			},
+		},
+	}
+	result := anonymizeInstallConfig(installConfig)
+
+	// Verify BMC credentials are anonymized
+	assert.NotNil(t, result.BareMetal)
+	assert.Len(t, result.BareMetal.Hosts, 2)
+	assert.NotEqual(t, "admin", result.BareMetal.Hosts[0].BMC.Username)
+	assert.NotEqual(t, "password123", result.BareMetal.Hosts[0].BMC.Password)
+	assert.NotEqual(t, "root", result.BareMetal.Hosts[1].BMC.Username)
+	assert.NotEqual(t, "secret456", result.BareMetal.Hosts[1].BMC.Password)
+	assert.NotEmpty(t, result.BareMetal.Hosts[0].BMC.Username)
+	assert.NotEmpty(t, result.BareMetal.Hosts[0].BMC.Password)
 }
